b0ef71c31a5891e37f3a52f5b1c72a31923540c9
Administrator <huoquan00022@163.com>
2023-03-16 09:53:40 +0800

    2023.3.16.9.53

--- joker.py
+++ joker.py
@@ -1,26 +1,140 @@
 import mitmproxy.http
 from mitmproxy import ctx, http
+import json,time
 
 
 class Joker:
+    def dumps(txt, beaut=0):
+        """ json序列化：dict -> json """
+        try:
+            if beaut:
+                txt = json.dumps(txt, sort_keys=True, indent=4, ensure_ascii=False)
+            else:
+                txt = json.dumps(txt, ensure_ascii=False)
+        except:
+            txt = txt
+        return txt
+
+
+    def loads(txt):
+        """ json反序列化：json -> dict """
+        try:
+            txt = json.loads(txt, encoding='UTF-8')
+        except:
+            txt = txt
+        return txt
+
+
+
     def request(self, flow: mitmproxy.http.HTTPFlow):
-        if flow.request.host != "www.baidu.com" or not flow.request.path.startswith("/s"):
-            ctx.log(flow.request.headers)
-            ctx.log(flow.request.url)
-            ctx.log(flow.request.text)
+        if flow.request.host == "operate-web.icrypton.com":
+            request_data,headers={},{}
+
+#            ctx.log("原始数据值是:%s",flow.request.content.decode('utf-8'))
+            request_data['method'] = flow.request.method
+            request_data['url'] = flow.request.url
+#            request_data['data']=flow.request.data
+            ctx.log("method 是:%s"%request_data['method'])
+            ctx.log("url是:%s"% request_data['url'])
+
+            #遍历请求头信息放入headers数组
+            for key, value in flow.request.headers.items():
+                headers[key] = value
+                ctx.log("headers key is:%s"%key)
+                ctx.log("headers value is:%s"%value)
+            request_data['headers'] = headers
+ #           ctx.log("请求头是:%s"%headers)
+
+            is_body = Joker.loads(flow.request.content.decode('utf-8'))
+            if is_body:
+                if isinstance(is_body,dict):     # isinstance() 函数，来判断一个函数是否是一个已知的类型
+                    body={'json':is_body}
+                else:
+                    body={'data':is_body}
+                request_data.update(body)
+            ctx.log("request_data是:%s"%request_data)
+
+
+
+        if flow.request.host=='www.hebeiza.cn':
+            request_data, headers,param = {}, {},{}
+
+
+            request_data['method'] = flow.request.method
+            #判断请求模式为get，获取get url 参数
+            if request_data['method'] =="GET":
+                for k, v in flow.request.query.items():
+                    # k：get url参数键
+                    print("参数键：%s"%k)
+                    # v：get url参数值
+                    print("参数值：%s"%v)
+                    param[k]=v
+
+            # 遍历请求头信息放入headers数组
+            for key, value in flow.request.headers.items():
+                # k：get url参数键
+                print("请求头参数键：%s" % key)
+                # v：get url参数值
+                print("请求头值：%s" % value)
+                headers[key] = value
+
+            #请求解析结束，json保存
+            request_data['method'] = flow.request.method
+            request_data['url'] = flow.request.url
+            request_data['headers'] = headers
+            request_data['param']=param
+            request_data=json.dumps(request_data,ensure_ascii=False)
+            print("处理后的值为：%s"%request_data)
+
+
+            is_body = Joker.loads(flow.request.content.decode('utf-8'))
+            if is_body:
+                if isinstance(is_body, dict):  # isinstance() 函数，来判断一个函数是否是一个已知的类型
+                    body = {'json': is_body}
+                else:
+                    body = {'data': is_body}
+                request_data.update(body)
+            ctx.log("request_data是:%s" % request_data)
+            #            request_data['data']=Joker.loads(flow.response.content.decode('utf-8'))
+
+
+            if flow.request.host == 'rdi.org.cn':
+                request_data, headers = {}, {}
+                # 请求信息组装
+                #            ctx.log("原始数据值是:%s",flow.request.content.decode('utf-8'))
+                request_data['method'] = flow.request.method
+                request_data['url'] = flow.request.url
+                #            request_data['data']=flow.request.data
+                ctx.log("method 是:%s" % request_data['method'])
+                ctx.log("url是:%s" % request_data['url'])
+                #            ctx.log("请求参数内容是：%s"%request_data['data'])
+                # 遍历请求头信息放入headers数组
+                for key, value in flow.request.headers.items():
+                    headers[key] = value
+                request_data['headers'] = headers
+                #           ctx.log("请求头是:%s"%headers)
+                #
+                is_body = Joker.loads(flow.request.content.decode('utf-8'))
+                if is_body:
+                    if isinstance(is_body, dict):  # isinstance() 函数，来判断一个函数是否是一个已知的类型
+                        body = {'json': is_body}
+                    else:
+                        body = {'data': is_body}
+                    request_data.update(body)
+                ctx.log("request_data是:%s" % request_data)
+                #            request_data['data']=Joker.loads(flow.response.content.decode('utf-8'))
+
             return
 
-        if "wd" not in flow.request.query.keys():
-            ctx.log.warn("can not get search word from %s" % flow.request.pretty_url)
-            return
-
-        ctx.log.info("catch search word: %s" % flow.request.query.get("wd"))
+#        if "wd" not in flow.request.query.keys():
+#            ctx.log.warn("can not get search word from %s" % flow.request.pretty_url)
+#            return
+#
+#        ctx.log.info("catch search word: %s" % flow.request.query.get("wd"))
     #    flow.request.query.set_all("wd", ["360搜索"])
 
     def response(self, flow: mitmproxy.http.HTTPFlow):
-        if flow.request.host != "www.so.com":
-            return
-
+        return
         text = flow.response.get_text()
         text = text.replace("搜索", "请使用谷歌")
         flow.response.set_text(text)
